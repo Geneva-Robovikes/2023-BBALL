ccc9396db9c9b7a74355fc909153bc73a196b769
Robovikes <robovikes@geneva304.org>
2022-09-22 17:17:32 -0500

    made project

---
+++ src/main/java/frc/robot/subsystems/BeltSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+public class BeltSubsystem extends SubsystemBase {  
+
+    private WPI_VictorSPX beltMotor;
+
+    public BeltSubsystem(){
+
+            beltMotor = new WPI_VictorSPX(5);
+
+    }
+
+    public void setBeltMotor() {
+        
+    }
+
+}
